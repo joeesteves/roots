cd576a3a55858bc33ed87a2d184cd9306c33d2f3
pony <ajesteves@gmail.com>
2014-07-28 00:32:15 -0300

    performance json no ejecuta validar_usuario y con Arbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,25 @@ class ApplicationController < ActionController::Base
 	
 
 	def conArbol
-		if action_name == 'index'
-			 @conArbol = Rba::Arbol.exists?(:modelo => controller_path)
+		unless request.format == 'application/json'
+			if action_name == 'index'
+				 @conArbol = Rba::Arbol.exists?(:modelo => controller_path)
+			end
 		end
 	end
 
 	def validar_usuario
-		url = request.original_url 
-		
-		if session[:usuario_id] == nil 
-			redirect_to root_url + '?url='+ url
-		else 				
-			@usuario_actual ||= Rba::Usuario.find(session[:usuario_id]) 
-			@e_grupo_actual = Rba::Empresagrupo.find(session[:empresagrupo_id])
-			@empresa_actual = Rba::Empresa.find(session[:empresa_id])
+		unless request.format == 'application/json'
+			url = request.original_url 
+			
+			if session[:usuario_id] == nil 
+				redirect_to root_url + '?url='+ url
+			else 				
+				@usuario_actual ||= Rba::Usuario.find(session[:usuario_id]) 
+				@e_grupo_actual = Rba::Empresagrupo.find(session[:empresagrupo_id])
+				@empresa_actual = Rba::Empresa.find(session[:empresa_id])
 
+			end
 		end
 	end	
 
--- app/models/rba/empresagrupo.rb
+++ app/models/rba/empresagrupo.rb
@@ -1,7 +1,7 @@
 class Rba::Empresagrupo < ActiveRecord::Base
 	include ModeloGlobales
 	habtm_nodo
-	has_many :empresas, :dependent => :restrict
+	has_many :empresas, :dependent => :restrict_with_exception
 	has_many :cuentas, :class_name => "Rco::Cuenta"
 
 end
